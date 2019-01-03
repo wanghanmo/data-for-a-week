59d0db06d67962ba33a24459580ff1302196b474
wanghanmo <37056631+wanghanmo@users.noreply.github.com>
2019-01-03 20:03:51 +0800

    day1.py
    将金融数据集三七分，随机种子2018，调用sklearn包，简单构建逻辑回归、SVM和决策树3个模型并对每一个模型进行评分，评分方式任意，例如准确度和auc值。

---
+++ day1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,roc_curve,auc
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+data=pd.read_csv('data_all.csv')
+
+y=data['status']
+X=data.drop(['status'],axis=1)
+print('the size of X,y:',X.shape,y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2018)
+
+models=[('LogisticRegression',LogisticRegression(random_state=2018)),('SVM',SVC(gamma='auto',random_state=2018)),('DecisionTreeClassifier',DecisionTreeClassifier(random_state=2018))]
+for name,model in models:
+    model.fit(X_train,y_train)
+    acc=accuracy_score(y_test,model.predict(X_test))
+    fpr, tpr, thresholds = roc_curve(y_test,model.predict(X_test))
+    model_auc=auc(fpr,tpr)
+    print(name,'测试集正确率,f1_score：',acc,model_auc)
